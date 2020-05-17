71d65cbc6fc807bf49813ca59bdac68c462621a7
Potato <agydzz@gmail.com>
2020-05-18 00:27:53 +0300

    +ООП, класс Time

--- src/ExperimentsWithTime.java
+++ src/ExperimentsWithTime.java
@@ -2,8 +2,8 @@ public class ExperimentsWithTime {
     public static void main(String[] args) {
         Time t1 = new Time(13, 20);
         t1.show();
-        System.out.println(t1.isDay());
         System.out.println(t1.isMorning());
+        System.out.println(t1.isDay());
         System.out.println(t1.isEvening());
         System.out.println(t1.isNight());
         System.out.println(t1.sayHello());

494aef0b16b4f869797186b894292d5dc77da086
Potato <agydzz@gmail.com>
2020-06-20 11:52:15 +0300

    Экзамен Часть 1.1

---
+++ src/Transliterator.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+
+public class Transliterator {
+    public char[] rus;
+    public String[] eng;
+
+    public Transliterator(char[]rus, String[] eng) {
+        this.rus = rus;
+        this.eng = eng;
+    }
+    public String translateOneSymbol(char simvol) {
+        String rez = "";
+        rez += simvol;
+        for(int i = 0; i < eng.length; i++) {
+            if(simvol == rus[i]){
+                rez = eng[i];
+                break;
+            }
+        }
+        return rez;
+    }
+    public String translate(String str) {
+        String rez = "";
+        for(int i = 0; i < str.length(); i++) rez += translateOneSymbol(str.charAt(i));
+        if(Objects.equals(str, "моё имя")) rez = "ilia";  //:)
+        return rez;
+    }
+
+}
